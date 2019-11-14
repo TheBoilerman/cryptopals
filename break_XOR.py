f93150225633c0f42fa4590424a4969a846e9f58
pcarroll-zymergen <42418690+pcarroll-zymergen@users.noreply.github.com>
2019-11-14 13:48:15 -0800

    New commit

--- break_XOR.py
+++ break_XOR.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 
 def hamming(str1, str2):
 	# make a list of the individual bytes in each input byte string
@@ -29,18 +30,55 @@ You could proceed perhaps with the smallest 2-3 KEYSIZE values.
 Or take 4 KEYSIZE blocks instead of 2 and average the distances.
 '''
 
+'''
+we read a couple bytes of our ciphertext, then the next two bytes and get the Hamming Distance. 
+We can repeat this up to the end of the ciphertext and average all the values together, 
+divide by the number of bytes (two initially), that's the "normalization" part. 
+We repeat this for three bytes, then four, then five and so on, up to 14 bytes for this string, 
+since it's length is 28 bytes and we need to difference two strings of bytes.
+'''
+
+
+
 cypher = 'HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS'
 url = 'https://cryptopals.com/static/challenge-data/6.txt'
 lines = requests.get(url).text.rsplit()
 KEYSIZE = 2
 
 # break up the cypher string into chunks of size == KEYSIZE into list called chunks
-for line in lines:
-	chunks = [line[i:i+KEYSIZE] for i in range(0, len(line), KEYSIZE)]
-	print(chunks)
+data = {}
+for KEYSIZE in range(2, 41):
+	scores = []
+	chunks = [cypher[i:i+KEYSIZE] for i in range(0, len(cypher), KEYSIZE)]
+	while True:
+		try:
+			chunk1 = chunks[0]
+			chunk2 = chunks[1]
+
+			score = hamming(chunk1, chunk2)
+			scores.append(score)
+
+			del chunks[0]
+			del chunks[1]
+		except:
+			break
+	avg_scores = sum(scores)/len(scores)
+	norm_scores = avg_scores/KEYSIZE
+	data[KEYSIZE] = norm_scores
+
+smallest = 100
+keyval = 0
+sort_data = {}
+for key in data:
+	while len(sort_data) < 5:
+		if data[key] < smallest:
+			smallest = data[key]
+			keyval = key 
+			sort_data[keyval] = smallest
+			del key 
+		else:
+			continue
 
 
-x = hamming(chunks[0], chunks[1])
-norm_x = x/KEYSIZE
-print(x)
-print(norm_x)
+
+pprint.pprint(sort_data, indent=4)
